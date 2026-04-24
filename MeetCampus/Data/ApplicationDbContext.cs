a440b241085161e4279c874c9c813137e21a089a
linyi <jianglinyi97@gmail.com>
2026-04-24 01:00:42 -0400

    seed more data

--- MeetCampus/Data/ApplicationDbContext.cs
+++ MeetCampus/Data/ApplicationDbContext.cs
@@ -130,6 +130,71 @@ namespace MeetCampus.Data
 
         private static void SeedProfileLookups(ModelBuilder builder)
         {
+            builder.Entity<StudyDomain>().HasData(
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000001"), Name = "Accounting" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000002"), Name = "Aerospace Engineering" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000003"), Name = "Architecture" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000004"), Name = "Art & Design" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000005"), Name = "Biology" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000006"), Name = "Business Administration" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000007"), Name = "Chemical Engineering" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000008"), Name = "Chemistry" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000009"), Name = "Civil Engineering" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000010"), Name = "Communications" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000011"), Name = "Computer Science" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000012"), Name = "Data Science" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000013"), Name = "Economics" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000014"), Name = "Education" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000015"), Name = "Electrical Engineering" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000016"), Name = "Environmental Science" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000017"), Name = "Film & Media Studies" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000018"), Name = "Finance" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000019"), Name = "Fine Arts" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000020"), Name = "Geography" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000021"), Name = "History" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000022"), Name = "Information Systems" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000023"), Name = "Journalism" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000024"), Name = "Kinesiology" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000025"), Name = "Law" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000026"), Name = "Linguistics" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000027"), Name = "Marketing" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000028"), Name = "Mathematics" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000029"), Name = "Mechanical Engineering" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000030"), Name = "Medicine" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000031"), Name = "Music" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000032"), Name = "Nursing" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000033"), Name = "Nutrition & Dietetics" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000034"), Name = "Pharmacy" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000035"), Name = "Philosophy" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000036"), Name = "Physics" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000037"), Name = "Political Science" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000038"), Name = "Psychology" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000039"), Name = "Public Health" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000040"), Name = "Social Work" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000041"), Name = "Sociology" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000042"), Name = "Software Engineering" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000043"), Name = "Statistics" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000044"), Name = "Theatre" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000045"), Name = "Urban Planning" },
+                new StudyDomain { Id = Guid.Parse("10000000-0000-0000-0000-000000000046"), Name = "Other" });
+
+            builder.Entity<School>().HasData(
+                new School { Id = Guid.Parse("20000000-0000-0000-0000-000000000001"), Name = "Concordia University" },
+                new School { Id = Guid.Parse("20000000-0000-0000-0000-000000000002"), Name = "McGill University" },
+                new School { Id = Guid.Parse("20000000-0000-0000-0000-000000000003"), Name = "Université du Québec à Montréal (UQAM)" },
+                new School { Id = Guid.Parse("20000000-0000-0000-0000-000000000004"), Name = "Université de Montréal (UdeM)" });
+
+            builder.Entity<Language>().HasData(
+                new Language { Id = Guid.Parse("70000000-0000-0000-0000-000000000001"), Name = "English" },
+                new Language { Id = Guid.Parse("70000000-0000-0000-0000-000000000002"), Name = "French" },
+                new Language { Id = Guid.Parse("70000000-0000-0000-0000-000000000003"), Name = "Mandarin Chinese" },
+                new Language { Id = Guid.Parse("70000000-0000-0000-0000-000000000004"), Name = "Hindi" },
+                new Language { Id = Guid.Parse("70000000-0000-0000-0000-000000000005"), Name = "Spanish" },
+                new Language { Id = Guid.Parse("70000000-0000-0000-0000-000000000006"), Name = "Arabic" },
+                new Language { Id = Guid.Parse("70000000-0000-0000-0000-000000000007"), Name = "Bengali" },
+                new Language { Id = Guid.Parse("70000000-0000-0000-0000-000000000008"), Name = "Portuguese" },
+                new Language { Id = Guid.Parse("70000000-0000-0000-0000-000000000009"), Name = "Russian" },
+                new Language { Id = Guid.Parse("70000000-0000-0000-0000-000000000010"), Name = "Urdu" });
             builder.Entity<UserIntention>().HasData(
                 new UserIntention
                 {
